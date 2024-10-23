8ed8f58658d93671125cae3913f95e20bfba47b3
Sidharth-A-691 <sidharth69122@gmail.com>
2024-10-23 17:31:44 +0530

    first

---
+++ src/main/java/com/ust/rest/ProductapiApplication.java
@@ -0,0 +1,12 @@
+package com.ust.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductapiApplication.class, args);
+	}
+}
